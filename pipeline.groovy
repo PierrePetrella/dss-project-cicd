bb601c9988c166c407492a3b633d07f9844af361
Pierre Petrella <pierre@petrella.fr>
2023-04-12 17:26:31 +0100

    quick test

--- pipeline.groovy
+++ pipeline.groovy
@@ -26,7 +26,7 @@ pipeline {
                 sh 'echo ${bundle_name}'
                 //sh "cat requirements.txt"
                 //git credentialsId: "git_hub_ssh", url: "git@github.com:PierrePetrella/dss-project-cicd.git"
-                withPythonEnv('python') {
+                withPythonEnv('python3') {
                     sh "pip install -U pip"
                     //sh "pip install -r ./requirements.txt"
                 }
