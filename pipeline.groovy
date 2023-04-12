ba6707d9624e0b1b31335dc856f213014cf3827c
Pierre Petrella <pierre@petrella.fr>
2023-04-12 16:15:32 +0100

    quick test

--- pipeline.groovy
+++ pipeline.groovy
@@ -20,7 +20,6 @@ pipeline {
                 sh 'virtualenv venv && . venv/bin/activate && pip install -r requirements.txt && python 1 + 1'
             }
         }
-
         stage('PREPARE'){
             steps {
                 cleanWs()
