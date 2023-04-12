4dfa6b36100e9af2adb18f0b215c9af33c8825b0
Pierre Petrella <pierre@petrella.fr>
2023-04-12 18:28:55 +0100

    quick test

--- pipeline.groovy
+++ pipeline.groovy
@@ -28,13 +28,13 @@ pipeline {
                 //git credentialsId: "git_hub_ssh", url: "git@github.com:PierrePetrella/dss-project-cicd.git"
 
                 sh "git clone ${GIT_REPO}"
-                sh "ls -la"
-                sh "cd dss-project-cicd"
-                sh "ls -la"
+                //sh "ls -la"
+                //sh "cd dss-project-cicd"
+                //sh "ls -la"
                 //sh "cat requirements.txt"
                 withPythonEnv('/Users/pierrepetrella/.pyenv/shims/python') {
                     sh "pip install -U pip"
-                    sh "cd dss-project-cicd && pip install -r requirements.txt"
+                    sh "pip install -r dss-project-cicd/requirements.txt"
                     //sh "pip install pandas==1.3.5"
                     //sh "pip install dataiku-api-client==11.4.0"
                     //sh "pip install http://localhost:12110/public/packages/dataiku-internal-client.tar.gz"
@@ -45,7 +45,7 @@ pipeline {
         stage('PROJECT_VALIDATION') {
             steps {
                 withPythonEnv('/Users/pierrepetrella/.pyenv/shims/python') {
-                    sh "pytest -s dss-project-pipeline/1_project_validation/run_test.py -o junit_family=xunit1 --host='${DESIGN_URL}' --api='${DESIGN_API_KEY}' --project='${DSS_PROJECT}' --junitxml=reports/PROJECT_VALIDATION.xml"
+                    sh "pytest -s dss-project-cicd/1_project_validation/run_test.py -o junit_family=xunit1 --host='${DESIGN_URL}' --api='${DESIGN_API_KEY}' --project='${DSS_PROJECT}' --junitxml=reports/PROJECT_VALIDATION.xml"
                 }
             }
         }
